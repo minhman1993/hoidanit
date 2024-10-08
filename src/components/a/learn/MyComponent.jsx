50ce8d80896b57520f7b8715986853abf8a9bead
bokid1412 <cangminhman@gmail.com>
2024-10-08 16:31:10 +0700

    video30

--- src/components/a/learn/MyComponent.jsx
+++ src/components/a/learn/MyComponent.jsx
@@ -1,5 +1,16 @@
-const MyComponent = () => {
-  return <div>MANTAROU</div>;
-};
+import './style.css';
 
+const MyComponent = () => {
+  return (
+    <>
+      <div>MANTAROU</div>;
+      <div
+        className='child'
+        style={{ borderRadius: '10px', backgroundColor: 'yellow' }}
+      >
+        RED
+      </div>
+    </>
+  );
+};
 export default MyComponent;
